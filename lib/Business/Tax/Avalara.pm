ea8a53af7cd6c563073c9359a6ec3b1ff9118f2f
Kate Kirby <kate@thinkgeek.com>
2012-10-12 09:42:48 -0400

    First pass of untested code. And hey look, POD!

--- lib/Business/Tax/Avalara.pm
+++ lib/Business/Tax/Avalara.pm
@@ -4,17 +4,65 @@ use strict;
 use warnings;
 
 use XML::LibXML qw();
+use XML::Hash;
 use Try::Tiny;
 use Carp;
+use LWP::UserAgent;
+
 
 =head1 NAME
 
 Business::Tax::Avalara - An interface to Avalara's REST webservice
 
+=head1 SYNOPSYS
+
+	use Business::Tax::Avalara;
+	my $avalara_gateway = Business::Tax::Avalara->new(
+		customer_code  => $customer_code,
+		company_code   => $company_code,
+		user_name      => $user_name,
+		password       => $password,
+		origin_address =>
+		{
+			line_1      => '1313 Mockingbird Lane',
+			postal_code => '98765',
+		},
+	);
+	
+	$avalara_gateway->get_tax(
+		destination_address =>
+		{
+			line_1      => '42 Evergreen Terrace',
+			city        => 'Springfield',
+			postal_code => '12345',
+		},
+		cart_lines =>
+		[
+			{
+				sku      => '42ACE',
+				quantity => 1,
+				amount   => '8.99',
+			},
+			{
+				sku      => '9FCE2',
+				quantity => 2,
+				amount   => '38.98',
+			}
+		],
+		
+	);
+	
 
 =head1 DESCRIPTION
 
-TODO
+Business::Tax::Avalara is a simple interface to Avalara's REST-based sales tax webservice.
+It takes in a perl hash of data to send to Avalara, generates the XML, fetches a response,
+and converts that back into a perl hash structure.
+
+Currently, json output is not supported, though Avalara can return that. (Feel free to
+let me know if anyone would find that useful.)
+
+This module only supports the 'get_tax' method at the moment.
 
 =head1 VERSION
 
@@ -23,8 +71,39 @@ Version 1.0.0
 =cut
 
 our $VERSION = '1.0.0';
-our $AVALARA_REQUEST_URL = 'https://rest.avalara.net/1.0/tax/get';
+our $AVALARA_REQUEST_SERVER = 'rest.avalara.net';
+our $AVALARA_DEVELOPMENT_REQUEST_SERVER = 'development.avalara.net';
 
+=head1 FUNCTIONS
+
+=head2 new()
+
+Creates a new Business::Tax::Avalara object with various options that do not change
+between requests.
+
+	my $avalara_gateway = Business::Tax::Avalara->new(
+		customer_code  => $customer_code,
+		company_code   => $company_code,
+		user_name      => $user_name
+		pasword        => $password,
+		is_development => boolean (optional), default 0
+		origin_address => $origin_address (optional),
+	);
+	
+The fields customer_code, company_code, user_name, and password should be
+provided by your Avalara representative. Account number and License key
+are synonyms for user_name and password, respectively.
+
+is_development should be set to 1 to use the development URL, and 0 for
+production uses.
+
+origin_address can either be set here, or passed into get_tax, depending on if
+it changes per request, or if you're always shipping from the same location.
+It is a hash ref, see below for formatting details.
+
+Returns a Business::Tax::Avalara object.
+
+=cut
 
 sub new
 {
@@ -33,9 +112,12 @@ sub new
 	# TODO: Require customer_code, company_code
 	
 	my $self = {
-		'customer_code' => $args{'customer_code'},
-		'company_code'  => $args{'company_code'},
-		'detail_level'  => $args{'detail_level'},
+		customer_code  => $args{'customer_code'},
+		company_code   => $args{'company_code'},
+		is_development => $args{'is_development'} // 0,
+		user_name      => $args{'user_name'},
+		password       => $args{'password'},
+		origin_address => $args{'origin_address'},
 	};
 	
 	bless $self, $class;
@@ -43,17 +125,50 @@ sub new
 }
 
 
+=head2 get_tax()
+
+Makes an XML request using the 'get_tax' method, parses the response, and returns a perl hash.
+
+	$avalara_gateway->get_tax(
+		destination_address   => $address_hash,
+		origin_address        => $address_hash (may be specified in new),
+		document_date         => $date (optional), default is current date
+		cart_lines            => $cart_line_hash,
+		customer_usage_type   => $customer_usage_type (optional),
+		discount              => $order_level_discount (optional),
+		purchase_order_number => $purchase_order_number (optional),
+		exemption_number      => $exemption_number (optional),
+		detail_level          => $detail_level (optional), default 'Tax',
+		document_type         => $document_type (optional), default 'SalesOrder'
+		payment_date          => $date (optional),
+		reference_code        => $reference_code (optional),
+		
+	);
+
+See below for the definitions of address and cart_line fields. The field origin_address
+may be specified here if it changes between transactions, or in new if it's largely static.
+
+detail level is one of 'Tax', 'Summary', 'Document', 'Line', or 'Diagnostic'.
+See the Avalara documentation for the distinctions.
+
+document_type is one of 'SalesOrder', 'SalesInvoice', 'PurchaseOrder', 'PurchaseInvoice',
+'ReturnOrder', and 'ReturnInvoice'.
+
+Returns a perl hashref based on the Avalara return.
+# TODO: Document the output.
+=cut
+
 sub get_tax
 {
 	my ( $self, %args ) = @_;
 	
-	# Perl nodeput, aka a hash ref, as opposed to XML or JSON.
-	my $tax_perl_nodeput = {};
+	# Perl output, aka a hash ref, as opposed to XML or JSON.
+	my $tax_perl_output = {};
 	try
 	{
-		my $request_xml = $self->generate_request_xml( %args );
-		my $result_xml = $self->make_request( $request_xml );
-		$tax_perl_nodeput = $self->parse_response_xml( $result_xml );
+		my $request_xml = $self->_generate_request_xml( %args );
+		my $result_xml = $self->_make_request( $request_xml );
+		$tax_perl_output = $self->_parse_response_xml( $result_xml );
 	}
 	catch
 	{
@@ -61,11 +176,21 @@ sub get_tax
 		return undef;
 	};
 	
-	return $tax_perl_nodeput;
+	return $tax_perl_output;
 }
 
 
-sub generate_request_xml
+=head1 INTERNAL FUNCTIONS
+
+=head2 _generate_request_xml()
+
+Generates the XML to send to Avalara's web service.
+
+Returns an XML DOM object.
+
+=cut
+
+sub _generate_request_xml
 {
 	my ( $self, %args ) = @_;
 	my $document_node = XML::LibXML->createDocument( '1.0', 'UTF-8' );
@@ -130,6 +255,32 @@ sub generate_request_xml
 }
 
 
+=head2 _generate_address_xml()
+
+Given an address hashref, generates and returns an address XML node.
+
+An address hashref is defined as:
+
+	my $address = {
+		line_1        => $first_line_of_address,
+		line_2        => $second_line_of_address,
+		line_3        => $third_line_of_address,
+		city          => $city,
+		region        => $state_or_province,
+		country       => $iso_2_code,
+		postal_code   => $postal_or_ZIP_code,
+		latitude      => $latitude,
+		longitude     => $longitude,
+		tax_region_id => $tax_region_id,
+	};
+	
+All fields are optional, though without enough to identify an address, your results will
+be less than satisfying.
+
+Country coes are ISO 3166-1 (alpha 2) format, such as 'US'.
+
+=cut
+
 sub _generate_address_xml
 {
 	my ( $self, $document_node, $address, $address_code ) = @_;
@@ -169,6 +320,30 @@ sub _generate_address_xml
 }
 
 
+=head2 _generate_cart_line_xml()
+
+Generates an XML node from a cart_line hashref. Cart lines are:
+
+	my $cart_line = {
+		'item_code'           => $item_code
+		'sku'                 => $sku, # Use sku OR item_code
+		'tax_code'            => $tax_code,
+		'customer_usage_type' => $customer_usage_code
+		'description'         => $description,
+		'quantity'            => $quantity,
+		'amount'              => $amount, # Extended price, ie, price * quantity
+		'discounted'          => $is_included_in_discount, # Boolean
+		'tax_included'        => $is_tax_included, # Boolean
+		'ref_1'               => $reference_1,
+		'ref_2'               => $reference_2,
+	}
+	
+One of item_code or sku, quantity, and amount are required fields.
+
+Customer usage type determines the type of item (sometimes called entity or use code). In some
+states, different types of items have different tax rates.
+
+=cut
 
 sub _generate_cart_line_xml
 {
@@ -216,6 +391,112 @@ sub _generate_cart_line_xml
 	}
 	
 	return $cart_line_node;
+}
+
+
+=head2 _make_request()
+
+Makes the https request to Avalara, and returns the response xml.
+
+=cut
+
+sub _make_request
+{
+	my ( $self, $request_xml ) = @_;
+	#  'https://rest.avalara.net/1.0/tax/get';
+	
+	
+	my $request_server = $self->{'is_development'}
+		? $AVALARA_DEVELOPMENT_REQUEST_SERVER
+		: $AVALARA_REQUEST_SERVER;
+	my $request_url = 'https://' . $request_server . '/1.0/tax/get';
+	
+	# Create a user agent object
+	my $user_agent = LWP::UserAgent->new();
+	$user_agent->agent( "perl/Business-Tax-Avalara/$VERSION" );
+	
+	# Set the httpd authentication credentials
+	$user_agent->credentials(
+		$request_server . ':80',
+		'1.0/tax/get',
+		$self->{'user_name'},
+		$self->{'password'},
+	);
+	
+	# Create a request
+	my $request = HTTP::Request->new(POST => $request_url);
+	$request->content_type('text/xml');
+	$request->content( $request_xml );
+	
+	# Pass request to the user agent and get a response back
+	my $response = $user_agent->request( $request );
+	
+	# Check the outcome of the response
+	if ( $response->is_success() )
+	{
+		return $response->content();
+	}
+	else
+	{
+		die "Failed to fetch XML response: " . $response->status_line() . "\n";
+	}
+	
+	return;
+}
+
+
+=head2 _parse_response_xml()
+
+Converts the returned XML into a perl hash.
+
+=cut
+
+sub _parse_response_xml
+{
+	my ( $self, $response_xml ) = @_;
+	my $response_hash = {};
+	
+	my $xml_document;
+	try
+	{
+		$xml_document = $self->_parse_xml( $response_xml );
+	}
+	catch
+	{
+		die "Failed to parse xml document: $_\n";
+	};
+	
+	my $xml_converter = XML::Hash->new();
+	$response_hash = $xml_converter->fromDOMtoHash( $xml_document );
+
+	return $response_hash;
+}
+
+
+=head2 _parse_xml()
+
+Parses the XML string into a DOM object.
+
+=cut
+
+sub _parse_xml
+{
+	my ( $self, $xml ) = @_;
+	
+	my $xml_document;
+	try
+	{
+		my $parser = XML::LibXML->new();
+		$xml_document = $parser->load_xml( string => $xml );
+	}
+	catch
+	{
+		die "Could not parse XML: $_\n";
+	};
+	
+	# TODO: Add some sanity checks here.
+	
+	return $xml_document;
 }
 
 
