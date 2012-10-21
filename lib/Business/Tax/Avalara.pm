2ec7b356e65533052445812b5a22410d2749e77b
Kate Kirby <kate@thinkgeek.com>
2012-10-20 21:41:41 -0400

    Build 1.0.1.

--- lib/Business/Tax/Avalara.pm
+++ lib/Business/Tax/Avalara.pm
@@ -67,11 +67,11 @@ This module only supports the 'get_tax' method at the moment.
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 our $AVALARA_REQUEST_SERVER = 'rest.avalara.net';
 our $AVALARA_DEVELOPMENT_REQUEST_SERVER = 'development.avalara.net';
 
